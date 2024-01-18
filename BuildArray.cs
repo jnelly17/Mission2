5f180f6e643b4a0fa0d1b1372f215da9bba6646b
jnelly17 <jaketnelson617@gmail.com>
2024-01-17 22:09:12 -0700

    new final

---
+++ BuildArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class BuildArray
+    {
+        public int[] ConfigArray(int numRolls)
+        {
+
+            int[] rolltotals = new int[13]; // 13 bc 2 is lowest and then can allocate exactly so 2-12
+
+            System.Random random = new System.Random();
+
+            //make a for loop for each roll
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = 0;
+                int die2 = 0;
+                int total = 0;
+
+                //roll the two dice
+
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                total = die1 + die2;
+
+                //add one to the tally for that corresponding number
+
+                rolltotals[total]++;
+            }
+
+            return rolltotals;
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
-﻿internal class Program
+﻿using Mission2;
+
+public class Program
 {
     private static void Main(string[] args)
     {
+        BuildArray ba = new BuildArray();
+
         int numRolls = 0;
 
         //ask user how many times they want to roll dice
         System.Console.WriteLine("Welcome to the dice throwing simulator!\nHow many dice rolls would you like to simulate?");
         numRolls = int.Parse(System.Console.ReadLine());
 
-        int[] diceArray = ConfigArray(numRolls);
+        int[] diceArray = ba.ConfigArray(numRolls);
 
         System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\n" +
             "Each \"*\" represents 1% of the total number of rolls\n" +
@@ -27,33 +31,4 @@
 
     }
 
-    private static int[] ConfigArray(int numRolls)
-    { 
-
-        int[] rolltotals = new int[13]; // 13 bc 2 is lowest and then can allocate exactly so 2-12
-
-        System.Random random = new System.Random();
-
-        //make a for loop for each roll
-        for (int i = 0; i < numRolls; i++)
-        {
-            int die1 = 0;
-            int die2 = 0;
-            int total = 0;
-
-            //roll the two dice
-
-            die1 = random.Next(1, 7);
-            die2 = random.Next(1, 7);
-            total = die1 + die2;
-
-            //add one to the tally for that corresponding number
-
-            rolltotals[total]++;
-        }
-
-        return rolltotals;
-
-
-    }
 }
